a1b7b4adef71ed68699b2cd5cca68d7fa200aa3d
balits-peter-marton <balits.peter.marton@gmail.com>
2022-11-18 19:53:28 +0100

    Added drawer for left sidebar. Also added NoHeaderLayout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,14 +1,18 @@
+import { DrawerProvider, NavDrawer, CartDrawer } from "../components/drawer"
 import Header from "../components/Header"
-import Seo from "../components/seo"
+import DefaultSeo from "../seo/DefaultSeo"
+import Link from "next/link"
 
 type LayoutChildren = { children: React.ReactNode }
 
 export const DefaultLayout: React.FC<LayoutChildren> = ({ children }) => {
   return (
-    <div className="relative w-screen bg-offwhite h-screen flex flex-col items-center justify-start ">
-      <Seo />
-      <Header />
-      <main className="h-fit w-full max-w-[1400px]">
+    <div className="relative max-w-screen bg-offwhite h-screen flex flex-col items-center justify-start ">
+      <DrawerProvider>
+        <Header />
+        <NavDrawer />
+      </DrawerProvider>
+      <main className="h-fit w-full px-3 xl:px-0 xl:max-w-[1400px] mt-header-base md:mt-header-md xl:mt-header-xl">
         {children}
       </main>
     </div>
@@ -17,8 +21,13 @@ export const DefaultLayout: React.FC<LayoutChildren> = ({ children }) => {
 
 export const NoHeaderLayout: React.FC<LayoutChildren> = ({ children }) => {
   return (
-    <div className="relative w-screen bg-offwhite h-screen flex flex-col items-center justify-start ">
-      <main className="h-fit w-full max-w-[1400px]">
+    <div className="relative w-screen bg-offwhite h-screen">
+      <header className="z-10 flex items-center justify-center  fixed top-0 h-header-base md:h-header-md xl:h-header-xl  w-full shadow-sm">
+        <Link href="/">
+          <p className="text-[16px] md:text-xl lg:text-[24px] xl:text-[28px] lg:pl-4 font-cantata font-bold">ART BY BORI</p>
+        </Link>
+      </header>
+      <main className="h-fit w-full px-3 xl:px-0 xl:max-w-[1400px] pt-header-base md:pt-header-md xl:pt-header-xl">
         {children}
       </main>
     </div>

092de25ae10bcb508aa1b07970889c53f7b95fca
balits-peter-marton <balits.peter.marton@gmail.com>
2022-11-18 19:54:41 +0100

    Formated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,23 @@
-
+import Link from "next/link"
+import { NoHeaderLayout } from "../src/layouts"
 
 const NotFound = () => {
-  return <h1>Not found</h1>
+  //TODO add head-title to not found page ("404 -404 -  Art by Bori")
+  return (
+    <NoHeaderLayout>
+      <section className="pt-28">
+        <h1 className="text-7xl text-stone-900 font-kameron font-semibold w-full text-center mb-8">
+          404
+        </h1>
+        <p className="font-hind text-4xl  text-darkGray text-center ">
+          Sadly the page you tried to visit simply doesn&apos;t exist.
+        </p>
+      </section>
+      <p className="text-xl text-center font-hind mx-auto w-[65vw] text-darkGray ">
+        Check if you searched for the intended URL, or try visiting a bit later. Additionaliy, check your Internet connection just to be sure.
+      </p>
+    </NoHeaderLayout>
+  )
 }
 
 NotFound.Layout = "nolayout"

4e8f75382bf2600910ac3a484427c903034dd8c7
balits-peter-marton <balits.peter.marton@gmail.com>
2022-11-19 15:53:10 +0100

    Resized text on Nav, styling has still a long way to go

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,8 +8,8 @@ const Nav: React.FC<NavProps> = ({ flexDirection }) => {
 
   const style = clsx(
     "font-kameron w-fit h-fit flex",
-    flexDirection === "row" && "items-center lg:text-xl xl:text-2xl lg:gap-x-14 xl:gap-x-11",
-    flexDirection === "col" && "flex-col items-start justify-center gap-y-12 text-xl gap-y-10 md:text-2xl"
+    flexDirection === "row" && "items-center text-md lg:text-xl lg:gap-x-14 xl:gap-x-11",
+    flexDirection === "col" && "flex-col items-start justify-center gap-y-12 text-xl"
   )
   const hideNavbar = clsx(flexDirection === "row" && "hidden lg:block")
   //in Header.tsx, if we're past the lg(1024px) breakpoint, we want to show our navbar
@@ -18,7 +18,7 @@ const Nav: React.FC<NavProps> = ({ flexDirection }) => {
   return (
     <nav className={hideNavbar}>
       <ul className={style}>
-        <li className=""><Link href="/portfolio">portfolio</Link></li>
+        <li><Link href="/portfolio">portfolio</Link></li>
         <li><Link href="/categories">categories</Link></li>
         <li><Link href="/products">products</Link></li>
         <li><Link href="/about">about</Link></li>

7a07b9e78f06a7066918cc545a3a621aa0fe150e
balits-peter-marton <balits.peter.marton@gmail.com>
2022-11-19 15:51:45 +0100

    Modified responsiveness; Removed unnecessary divs and relative/w-screen h-screen classes, for a more readable layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,21 @@
 import Link from "next/link"
 import { useContext } from "react"
-import { CartDrawerContext, NavDrawerContext } from "./drawer"
+import { NavDrawerContext } from "./drawer"
 import Nav from "./Nav"
 
 const Header: React.FC = () => {
   const { onOpen: navOnOpen, open: navOpen } = useContext(NavDrawerContext)
   //const { onOpen: cartOnOpen } = useContext(CartDrawerContext)
 
-
   return (
-    <header className="fixed top-0 flex items-center justify-between filter backdrop-blur-lg bg-offwhite/70 w-full max-w-[1500px] z-30 h-header-base md:h-header-md xl:h-header-xl">
+    <header className="fixed top-0 flex items-center justify-between filter backdrop-blur-lg bg-offwhite/70 w-full max-w-[1500px] z-30 h-header-base md:h-header-md xl:h-header-xl lg:px-2 ">
       <button className="lg:hidden p-2 md:p-3 lg:p-4" aria-label="Open navigation panel from the left side." onClick={navOnOpen}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 md:h-6 md:w-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
         </svg>
       </button>
       <Link href="/">
-        <p className="text-[16px] md:text-xl lg:text-[24px] xl:text-[28px] lg:pl-4 font-cantata font-bold">ART BY BORI</p>
+        <p className="text-[16px] md:text-xl lg:text-[22px] xl:text-[26px]  font-cantata font-bold">ART BY BORI</p>
       </Link>
       <Nav flexDirection="row" />
       <button className="p-2 md:p-3 lg:p-4" aria-label="Open cart panel from the right side.">
--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -57,7 +57,7 @@ export const NavDrawer: React.FC = () => {
             exit={{ opacity: 0 }}
             onClick={onClose}
           />
-          <motion.div className="z-50 bg-offwhite absolute inset-0 h-full w-[65vw] md:w-[40vw] pr-4 pl-6  md:pl-8 mt:pt-8 flex items-start justify-start flex-col gap-y-20"
+          <motion.div className="z-50 bg-offwhite absolute inset-0 h-full w-[55vw] md:w-[35vw] pr-4 pl-6  md:pl-8 mt:pt-8 flex items-start justify-start flex-col gap-y-20"
             initial={{ x: -1000 }}
             animate={{ x: 0 }}
             exit={{ x: -1000 }}
@@ -67,14 +67,14 @@ export const NavDrawer: React.FC = () => {
               delay: 0.2
             }}
           >
-            <section className="flex items-center justify-between w-full h-16 md:h-20 xl:h-24">
-              <p className=" text-[20px] md:text-xl lg:text-[24px] xl:text-[28px] font-cantata font-bold">ART BY BORI</p>
+            <div className="flex items-center justify-between w-full h-16 md:h-20 xl:h-24">
+              <p className="text-base md:text-xl lg:text-[24px] xl:text-[28px] font-cantata font-bold">ART BY BORI</p>
               <button aria-label="Close navigation panel." onClick={onClose}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 md:h-6 md:w-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
-            </section>
+            </div>
             <Nav flexDirection="col" />
           </motion.div>
         </>

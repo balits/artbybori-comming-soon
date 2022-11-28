10df118912c95339c6947d13e598ed3766e7a9d2
balits-peter-marton <balits.peter.marton@gmail.com>
2022-11-28 14:47:03 +0100

    refactoring

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <p className="text-[16px] md:text-xl lg:text-[24px] xl:text-[28px] lg:pl-4 font-cantata font-bold">ART BY BORI</p>
+    </Link>
+  )
+}

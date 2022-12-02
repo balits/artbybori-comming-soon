59746ae3ef70e97ad6bc9dd75737ca6822085afb
balits-peter-marton <balits.peter.marton@gmail.com>
2022-12-02 21:43:14 +0100

    coming-soon 3: fixed deployment issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
-import { Cantata_One } from "@next/font/google"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
